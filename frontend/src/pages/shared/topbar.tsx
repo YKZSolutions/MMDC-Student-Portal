45d0a4caf395c8b8415fc56a7a5d96631e107b64
Harvey Dela Flor <lr.hdflor@mmdc.mcl.edu.ph>
2025-08-03 23:36:29 +0800

    fixed missing pages

---
+++ frontend/src/pages/shared/topbar.tsx
@@ -0,0 +1,73 @@
+import { ActionIcon, Breadcrumbs, Group, Text } from '@mantine/core'
+import {
+  IconBell,
+  IconChevronRight,
+  IconHomeFilled,
+  IconMessageChatbot,
+} from '@tabler/icons-react'
+import { Link, useLocation } from '@tanstack/react-router'
+
+function Topbar() {
+  const location = useLocation()
+
+  const paths = location.pathname.split('/').slice(1)
+  const pathLinks = paths.map((_, i) => '/' + paths.slice(0, i + 1).join('/'))
+
+  return (
+    <Group className="px-4 py-2" justify="space-between" align="center">
+      <Breadcrumbs
+        c="dimmed"
+        separator={<IconChevronRight size={14} color="gray" />}
+      >
+        <ActionIcon radius="xl" size="sm">
+          <IconHomeFilled size={14} />
+        </ActionIcon>
+
+        {paths.map((link, idx) => {
+          const isLast = idx === paths.length - 1
+          const path = pathLinks[idx]
+
+          const label = link.charAt(0).toUpperCase() + link.slice(1)
+
+          return <BreadcrumbItem isLast={isLast} path={path} label={label} />
+        })}
+      </Breadcrumbs>
+
+      <Group>
+        <ActionIcon variant="subtle" radius="xl">
+          <IconMessageChatbot size={22} />
+        </ActionIcon>
+        <ActionIcon variant="subtle" radius="xl">
+          <IconBell size={22} />
+        </ActionIcon>
+      </Group>
+    </Group>
+  )
+}
+
+interface BreadcrumbItemProps {
+  isLast: boolean
+  path: string
+  label: string
+}
+
+function BreadcrumbItem({ isLast, path, label }: BreadcrumbItemProps) {
+  return isLast ? (
+    <Text key={path} size="sm" c={'dark.5'} fw="bold" className="capitalize">
+      {label}
+    </Text>
+  ) : (
+    <Text
+      key={path}
+      size="sm"
+      component={Link}
+      to={path}
+      className="capitalize"
+      style={{ textDecoration: 'none' }}
+    >
+      {label}
+    </Text>
+  )
+}
+
+export default Topbar
