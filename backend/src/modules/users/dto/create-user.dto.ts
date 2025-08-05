1200579f49ac14cd3804ddf3977b84d728d15614
Kristopher Santos <topers1820@gmail.com>
2025-08-06 04:53:03 +0800

    Refactored the create & update user

--- backend/src/modules/users/dto/create-user.dto.ts
+++ backend/src/modules/users/dto/create-user.dto.ts
@@ -1,11 +1,64 @@
 import { Type } from 'class-transformer';
-import { IsOptional, ValidateNested } from 'class-validator';
+import {
+  IsEnum,
+  IsNotEmpty,
+  IsOptional,
+  ValidateNested,
+} from 'class-validator';
 import { UserCredentialsDto } from './user-credentials.dto';
 import { CreateUserDto } from '@/generated/nestjs-dto/create-user.dto';
+import { CreateUserDetailsDto } from '@/generated/nestjs-dto/create-userDetails.dto';
+import { CreateStudentDetailsDto } from '@/generated/nestjs-dto/create-studentDetails.dto';
+import { CreateStaffDetailsDto } from '@/generated/nestjs-dto/create-staffDetails.dto';
+import { ApiProperty, OmitType } from '@nestjs/swagger';
+import { Role } from '@prisma/client';
 
-export class CreateUserWithAccountDto extends CreateUserDto {
+export class CreateUserDtoNoRole extends OmitType(CreateUserDto, [
+  'role',
+] as const) {}
+
+export class CreateUserBaseDto {
+  @ValidateNested()
+  @Type(() => CreateUserDtoNoRole)
+  user: CreateUserDtoNoRole;
+
   @IsOptional()
   @ValidateNested()
   @Type(() => UserCredentialsDto)
   credentials?: UserCredentialsDto;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => CreateUserDetailsDto)
+  userDetails?: CreateUserDetailsDto;
+}
+
+export class CreateUserFullDto extends CreateUserBaseDto {
+  @ApiProperty({
+    enum: Role,
+    enumName: 'Role',
+  })
+  @IsNotEmpty()
+  @IsEnum(Role)
+  role: Role;
+}
+
+export class CreateUserStudentDto extends CreateUserBaseDto {
+  @ValidateNested()
+  @Type(() => CreateStudentDetailsDto)
+  specificDetails: CreateStudentDetailsDto;
+}
+
+export class CreateUserStaffDto extends CreateUserBaseDto {
+  @ApiProperty({
+    enum: ['mentor', 'admin'],
+    enumName: 'StaffRole',
+  })
+  @IsNotEmpty()
+  @IsEnum(['mentor', 'admin'])
+  role: Omit<Role, 'student'>;
+
+  @ValidateNested()
+  @Type(() => CreateStaffDetailsDto)
+  specificDetails: CreateStaffDetailsDto;
 }
--- backend/src/modules/users/dto/update-user-details.dto.ts
+++ backend/src/modules/users/dto/update-user-details.dto.ts
@@ -1,4 +1,36 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from '@/generated/nestjs-dto/create-user.dto';
+import { OmitType } from '@nestjs/swagger';
+import { UpdateUserDto } from '@/generated/nestjs-dto/update-user.dto';
+import { IsEnum, IsOptional, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+import { UpdateUserDetailsDto } from '@/generated/nestjs-dto/update-userDetails.dto';
+import { UpdateStudentDetailsDto } from '@/generated/nestjs-dto/update-studentDetails.dto';
+import { UpdateStaffDetailsDto } from '@/generated/nestjs-dto/update-staffDetails.dto';
 
-export class UpdateUserDetailsDto extends PartialType(CreateUserDto) {}
+export class UpdateUserNoRoleDto extends OmitType(UpdateUserDto, [
+  'role',
+] as const) {}
+
+export class UpdateUserBaseDto {
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => UpdateUserNoRoleDto)
+  user?: UpdateUserNoRoleDto;
+
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => UpdateUserDetailsDto)
+  userDetails?: UpdateUserDetailsDto;
+}
+
+export class UpdateUserStudentDto extends UpdateUserBaseDto {
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => UpdateStudentDetailsDto)
+  specificDetails?: UpdateStudentDetailsDto;
+}
+export class UpdateUserStaffDto extends UpdateUserBaseDto {
+  @IsOptional()
+  @ValidateNested()
+  @Type(() => UpdateStaffDetailsDto)
+  specificDetails?: UpdateStaffDetailsDto;
+}
