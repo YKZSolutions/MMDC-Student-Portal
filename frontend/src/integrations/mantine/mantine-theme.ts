d06dc6711a93282c434099509724487a9d58ab35
Harvey Dela Flor <harvey.delaflor@gmail.com>
2025-08-08 02:04:24 +0800

    Update Mantine theme to adjust secondary color and include `secondaryColor` in theme configuration

--- frontend/src/integrations/mantine/mantine-theme.ts
+++ frontend/src/integrations/mantine/mantine-theme.ts
@@ -8,7 +8,7 @@ import {
 const customColors = {
   background: colorsTuple('#FAFAFA'),
   primary: colorsTuple('#123373'),
-  secondary: colorsTuple('#F80507'),
+  secondary: colorsTuple('#BE0000'),
 } satisfies Record<string, MantineColorsTuple>
 
 type CustomColors = keyof typeof customColors
@@ -24,6 +24,7 @@ declare module '@mantine/core' {
 
 export const theme = createTheme({
   primaryColor: 'primary',
+  secondaryColor: 'secondary',
   colors: {
     ...customColors,
   },
