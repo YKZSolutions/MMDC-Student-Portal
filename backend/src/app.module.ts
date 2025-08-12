f01a8b62187f0824b05d9a4bcae6957d808baae2
Harvey Dela Flor <harvey.delaflor@gmail.com>
2025-08-12 14:24:33 +0800

    Apply `RequestIdMiddleware` to all routes in `AppModule` for consistent request tracking.

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { APP_GUARD } from '@nestjs/core';
 import { CustomPrismaModule } from 'nestjs-prisma';
 import { AuthGuard } from './common/guards/auth.guard';
@@ -11,6 +11,7 @@ import { TestModule } from './modules/test/test.module';
 import { UsersModule } from './modules/users/users.module';
 import { UserStatusGuard } from './common/guards/user-status.guard';
 import { AuthModule } from './modules/auth/auth.module';
+import { RequestIdMiddleware } from '@/middleware/request-id.middleware';
 
 @Module({
   imports: [
@@ -47,4 +48,8 @@ import { AuthModule } from './modules/auth/auth.module';
     },
   ],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(RequestIdMiddleware).forRoutes('*');
+  }
+}
