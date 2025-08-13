ef7998696df2938c1afb9f47a427d8c612f17a35
Harvey Dela Flor <harvey.delaflor@gmail.com>
2025-08-13 15:22:54 +0800

    Refactor `PromptDto` to include validation decorators and restructure user context into `UserBaseContextDto`, `UserStudentContextDto`, and `UserStaffContextDto`.

--- backend/src/modules/chatbot/dto/prompt.dto.ts
+++ backend/src/modules/chatbot/dto/prompt.dto.ts
@@ -1,10 +1,48 @@
-import {
-  UserStaffDetailsDto,
-  UserStudentDetailsDto,
-} from '@/modules/users/dto/user-details.dto';
+import { IsArray, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
 
 export class PromptDto {
-  sessionId: string;
-  prompt: string;
-  user: UserStudentDetailsDto | UserStaffDetailsDto;
+  @IsNotEmpty()
+  @IsString()
+  question: string;
+
+  @IsArray()
+  messageHistory: string[];
+
+  @ValidateNested()
+  @IsNotEmpty()
+  user: UserStudentContextDto | UserStaffContextDto;
+}
+
+export class UserBaseContextDto {
+  @IsNotEmpty()
+  @IsString()
+  id: string;
+
+  @IsNotEmpty()
+  @IsString()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  role: string;
+}
+
+export class UserStudentContextDto extends UserBaseContextDto {
+  @IsNotEmpty()
+  @IsString()
+  studentNumber: string;
+}
+
+export class UserStaffContextDto extends UserBaseContextDto {
+  @IsNotEmpty()
+  @IsString()
+  employeeNumber: string;
+
+  @IsNotEmpty()
+  @IsString()
+  department: string;
+
+  @IsNotEmpty()
+  @IsString()
+  position: string;
 }
