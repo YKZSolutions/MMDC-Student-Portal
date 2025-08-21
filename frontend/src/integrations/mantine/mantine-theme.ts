2f8541689b5004b6481947d0f789a5da1b658fea
Harvey Dela Flor <harvey.delaflor@gmail.com>
2025-08-21 21:09:27 +0800

    Extended Mantine theme with submission status colors: added color mappings for various statuses and updated type definitions.

--- frontend/src/integrations/mantine/mantine-theme.ts
+++ frontend/src/integrations/mantine/mantine-theme.ts
@@ -11,10 +11,18 @@ const customColors = {
   secondary: colorsTuple('#BE0000'),
 } satisfies Record<string, MantineColorsTuple>
 
+const submissionStatusColors = {
+  completed: colorsTuple('#008000'),
+  pending: colorsTuple('#FFA500'),
+  late: colorsTuple('#FF0000'),
+  locked: colorsTuple('#808080'),
+}
+
 type CustomColors = keyof typeof customColors
+type SubmissionStatusColors = keyof typeof submissionStatusColors
 
 // Merge the Mantine's built in colors ya dig?
-type ExtendedColors = DefaultMantineColor | CustomColors
+type ExtendedColors = DefaultMantineColor | CustomColors | SubmissionStatusColors
 
 declare module '@mantine/core' {
   export interface MantineThemeColorsOverride {
@@ -26,5 +34,6 @@ export const theme = createTheme({
   primaryColor: 'primary',
   colors: {
     ...customColors,
+    ...submissionStatusColors
   },
 })
