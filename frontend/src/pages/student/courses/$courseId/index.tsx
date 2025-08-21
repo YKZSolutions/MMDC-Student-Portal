065bf6c38a8809490fae9cef4bf5bf05b5f79026
Harvey Dela Flor <harvey.delaflor@gmail.com>
2025-08-21 22:06:29 +0800

    Added `CourseOverviewStudentView` component: draft.

---
+++ frontend/src/pages/student/courses/$courseId/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Container } from '@mantine/core'
+
+const CourseOverviewStudentView = () => {
+  return (
+    <Container>
+      Course Overview (contains course details, syllabus, mentor details, etc.)
+    </Container>
+  )
+}
+
+export default CourseOverviewStudentView
--- frontend/src/routes/(protected)/courses/$courseId/index.tsx
+++ frontend/src/routes/(protected)/courses/$courseId/index.tsx
@@ -1,4 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
+import CourseOverviewStudentView from '@/pages/student/courses/$courseId'
+import RoleComponentManager from '@/components/role-component-manager.tsx'
+import { useAuth } from '@/features/auth/auth.hook.ts'
 
 export const Route = createFileRoute(
   '/(protected)/courses/$courseId/',
@@ -7,5 +10,14 @@ export const Route = createFileRoute(
 })
 
 function RouteComponent() {
-  return <div>Hello "/(protected)/courses/$courseId/overview/"!</div>
+  const { authUser } = useAuth('protected')
+
+  return (
+    <RoleComponentManager
+      currentRole={authUser.role}
+      roleRender={{
+        student: <CourseOverviewStudentView />,
+      }}
+    />
+  )
 }
