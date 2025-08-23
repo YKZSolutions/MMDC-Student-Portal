ce8a1ea0f9e0f784f25215b6f58d1d0f40c22f0c
Harvey Dela Flor <harvey.delaflor@gmail.com>
2025-08-24 01:55:18 +0800

    Added `ContentDetailsEditor` component with initial layout, placeholder fields, and basic structure for content editing in the course editor.

---
+++ frontend/src/features/courses/course-editor/content-details-editor.tsx
@@ -0,0 +1,58 @@
+import {
+  ActionIcon,
+  Stack,
+  Textarea,
+  useMantineTheme,
+} from '@mantine/core'
+import FormRow from '@/components/form-row.tsx'
+import {
+  IconCategory,
+  IconChevronsRight,
+} from '@tabler/icons-react'
+import type { ComponentPropsWithoutRef } from 'react';
+
+type ContentDetailsEditorProps = {
+  opened: boolean;
+  onClose: () => void;
+  type: string;
+  data: any;
+} & ComponentPropsWithoutRef<typeof Stack>
+
+// Originally Used as a Right Pane details editor
+// TODO: Add expanded state, and render according to type
+const ContentDetailsEditor = ({opened, onClose, type, data, ...stackProps}: ContentDetailsEditorProps) => {
+  const theme = useMantineTheme();
+  return (
+    <Stack pos="relative" {...stackProps}>
+        <ActionIcon
+            onClick={onClose}
+            variant="transparent"
+            size="lg"
+            color="gray"
+            style={{ position: 'absolute', top: theme.spacing.xs, left: theme.spacing.xs }}
+        >
+            <IconChevronsRight size={32} />
+        </ActionIcon>
+
+        <Textarea
+            size='2rem'
+            inputSize='1rem'
+            fw={700} mt={'sm'}
+            placeholder="Enter Title"
+            variant={'unstyled'}
+            autosize
+            minRows={1}
+            maxRows={2}
+        />
+        {/*TODO: add actual content types, these are just placeholders*/}
+        <Stack gap={'sm'}>
+            <FormRow label="Category" />
+            <FormRow label="Title" icon={<IconCategory />} />
+            <FormRow label="Description" placeholder="Enter description" />
+            <FormRow label="Tags" />
+        </Stack>
+    </Stack>
+  )
+}
+
+export default ContentDetailsEditor
