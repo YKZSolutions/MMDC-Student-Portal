b3604ff25c140d2f26e74b1459ef6057ddec1a46
Harvey Dela Flor <harvey.delaflor@gmail.com>
2025-08-23 23:07:05 +0800

    Added `RoleBasedActionButton` component for role-specific actions with customizable icons, text, and behavior.

---
+++ frontend/src/components/role-based-action-button.tsx
@@ -0,0 +1,47 @@
+import { useAuth } from '@/features/auth/auth.hook.ts'
+import RoleComponentManager from '@/components/role-component-manager.tsx'
+import { Button } from '@mantine/core'
+import { IconEdit, IconVideo } from '@tabler/icons-react'
+import React from 'react'
+import type { Role } from '@/integrations/api/client'
+
+type DefaultRoles = Role
+
+type RoleBasedActionButtonProps = {
+  disabled: boolean;
+  render: {
+    [K in DefaultRoles]?: {
+      icon: React.ReactNode;
+      text: string;
+      click: () => void;
+    }
+  }
+}
+
+const RoleBasedActionButton = ({
+  disabled,
+  render,
+}: RoleBasedActionButtonProps) => {
+  const { authUser } = useAuth('protected')
+  const { role } = authUser
+  const { icon, text, click } = render[role] || {}
+
+  if (!icon || !text || !click) {
+    return null
+  }
+
+  return (
+    <Button
+      leftSection={icon}
+      size="xs"
+      radius="xl"
+      variant="filled"
+      disabled={disabled}
+      onClick={click}
+    >
+      {text}
+    </Button>
+  )
+}
+
+export default RoleBasedActionButton
