14c2ddef0be95e76a4e92c5a4c1e825d42cdd46f
Morfusee <markvalenzuela.coding@gmail.com>
2025-08-24 18:51:18 +0800

    fix: removed staleTime for caches

--- frontend/src/integrations/tanstack-query/root-provider.tsx
+++ frontend/src/integrations/tanstack-query/root-provider.tsx
@@ -1,12 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      staleTime: 1000 * 60 * 5,
-    },
-  },
-})
+const queryClient = new QueryClient()
 
 export function getContext() {
   return {
