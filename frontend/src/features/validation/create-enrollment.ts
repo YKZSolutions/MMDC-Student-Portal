a51d2b6be1ae81975b9d3daab55e889466b190b3
Morfusee <markvalenzuela.coding@gmail.com>
2025-08-26 21:00:32 +0800

    feat (zod): created zod obj for form validation

---
+++ frontend/src/features/validation/create-enrollment.ts
@@ -0,0 +1,48 @@
+import { zCreateEnrollmentPeriodDto } from '@/integrations/api/client/zod.gen'
+import z from 'zod'
+
+const zodEnrollmentPeriodCreate = zCreateEnrollmentPeriodDto.shape
+
+const zodStatusEnum = z.enum(zodEnrollmentPeriodCreate.status.options, {
+  error: 'Invalid enrollment status.',
+})
+
+export const CreateEnrollmentPeriodFormSchema = z
+  .object({
+    ...zodEnrollmentPeriodCreate,
+    endDate: z.date(),
+    endYear: z
+      .number()
+      .min(
+        new Date().getFullYear(),
+        'End year must be at least the current year',
+      ),
+    startDate: z.date(),
+    startYear: z
+      .number()
+      .min(
+        new Date().getFullYear(),
+        'Start year must be at least the current year',
+      ),
+    term: z.number().min(1, 'Term must be at least 1'),
+    status: zodStatusEnum,
+  })
+  .superRefine((data, ctx) => {
+    if (data.startDate >= data.endDate) {
+      ctx.addIssue({
+        path: ['startDate'],
+        code: 'custom',
+        message: 'Start date must be before end date.',
+      })
+
+      ctx.addIssue({
+        path: ['endDate'],
+        code: 'custom',
+        message: 'End date must be after start date.',
+      })
+    }
+  })
+
+export type CreateEnrollmentPeriodFormValues = z.infer<
+  typeof CreateEnrollmentPeriodFormSchema
+>
