a8e5778fd2e7683d3a43deb1b3f64d29715a76ce
Harvey Dela Flor <harvey.delaflor@gmail.com>
2025-08-27 02:11:57 +0800

    refactor(course-modules): clean up unused imports
    Removes a large number of unused imports from the `course-modules.tsx`
    component, improving readability and reducing unnecessary dependencies.

--- frontend/src/pages/shared/courses/$courseId/modules/course-modules.tsx
+++ frontend/src/pages/shared/courses/$courseId/modules/course-modules.tsx
@@ -1,55 +1,14 @@
+import { Button, Group, Stack } from '@mantine/core'
 import {
-  Group,
-  Stack,
-  Title,
-  Button,
-  Text,
-  Accordion,
-  useMantineTheme,
-  ActionIcon,
-  Menu,
-  type AccordionControlProps,
-  type GroupProps,
-} from '@mantine/core'
-import {
-  IconBook,
-  IconCircle,
-  IconCircleCheck,
-  IconDotsVertical,
-  IconEdit,
-  IconFile,
-  IconFlag,
-  IconLock,
-  IconPencil,
   IconPlus,
-  IconSettings,
-  IconTrash,
-  IconUrgent,
   IconViewportShort,
   IconViewportTall,
-  IconWriting,
 } from '@tabler/icons-react'
-import { useState, useEffect } from 'react'
-import SubmitButton from '@/components/submit-button.tsx'
-import RoleComponentManager from '@/components/role-component-manager.tsx'
+import { useState } from 'react'
 import { useAuth } from '@/features/auth/auth.hook.ts'
 import ButtonWithModal from '@/components/btn-w-modal.tsx'
 import CourseCreationProcessModal from '@/features/courses/course-editor/course-creation-process-modal.tsx'
-import type {
-  Assignment,
-  AssignmentType,
-  StudentAssignment,
-  SubmissionStatus,
-} from '@/features/courses/assignments/types.ts'
-import { getSubmissionStatus } from '@/utils/helpers.ts'
-import { mockAssignmentsData } from '@/pages/shared/courses/$courseId/assignments/course-assignments.tsx'
-import type { ContentType } from '@/features/courses/types.ts'
-import {
-  ModuleItemIcon,
-  CompletedStatusIcon,
-} from '@/components/icon-selector.tsx'
 import CourseMainLayout from '@/features/courses/course-main-layout.tsx'
-import { formatTimestampToDateTimeText } from '@/utils/formatters.ts'
 import CourseModulePanel from '@/features/courses/modules/course-module-panel.tsx'
 
 const CourseModules = () => {
