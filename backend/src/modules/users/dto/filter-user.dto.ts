64f1745692ab53a5fb773df1281910393e3aedff
Morfusee <markvalenzuela.coding@gmail.com>
2025-08-27 19:40:53 +0800

    fix (dto): user role filtering broke

--- backend/src/modules/users/dto/filter-user.dto.ts
+++ backend/src/modules/users/dto/filter-user.dto.ts
@@ -1,8 +1,11 @@
 import { BaseFilterDto } from '@/common/dto/base-filter.dto';
 import { ApiProperty } from '@nestjs/swagger';
 import { Role } from '@prisma/client';
+import { IsEnum, IsOptional } from 'class-validator';
 
 export class FilterUserDto extends BaseFilterDto {
+  @IsEnum(Role)
+  @IsOptional()
   @ApiProperty({ enum: Role, required: false })
   role?: Role;
 
