1d380e67982732d18d8aae93de2b757039945748
ibrahim <iharby2002@gmail.com>
2025-08-30 19:48:48 +0800

    Refactor: make studentId optional in StudentIdentifierDto

--- backend/src/modules/enrollment/dto/studentIdentifier.dto.ts
+++ backend/src/modules/enrollment/dto/studentIdentifier.dto.ts
@@ -1,7 +1,7 @@
-import { IsNotEmpty, IsUUID } from 'class-validator';
+import { IsOptional, IsUUID } from 'class-validator';
 
 export class StudentIdentifierDto {
-  @IsNotEmpty()
+  @IsOptional()
   @IsUUID('4')
-  studentId: string;
+  studentId?: string;
 }
