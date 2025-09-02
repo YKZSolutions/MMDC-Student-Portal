d9dc2e6f1a67db6757a92940abd430c9bbb63418
Harvey Dela Flor <harvey.delaflor@gmail.com>
2025-09-02 19:28:28 +0800

    feat(cms): reorganize layout for dynamic tree panel
    Moves course selection and tree toggle based on tree panel
    visibility. Adds `Tooltip` to 'Add Content' button.

--- frontend/src/routes/(protected)/cms/index.tsx
+++ frontend/src/routes/(protected)/cms/index.tsx
@@ -11,10 +11,12 @@ import {
   Stack,
   Text,
   Title,
+  Tooltip,
   useMantineTheme,
 } from '@mantine/core'
 import { Panel, PanelGroup, PanelResizeHandle } from 'react-resizable-panels'
 import {
+  IconBook,
   IconCalendar,
   IconChevronDown,
   IconGripVertical,
@@ -129,164 +131,191 @@ const CMS = ({ courseCode }: CMSProps) => {
 
   return (
     <Box h={'100%'} w={'100%'}>
-      <Stack flex={'1 0 auto'} h={'100%'} w={'100%'} gap={0}>
-        <Group
-          justify="space-between"
-          align="center"
-          w={'100%'}
-          pb={'sm'}
-          style={{ borderBottom: `2px solid ${theme.colors.gray[3]}` }}
-        >
-          <Select
-            data={mockCourseBasicDetails.map((course) => course.courseName)}
-            value={courseDetails?.courseName}
-            onChange={(value) => {
-              const course = mockCourseBasicDetails.find(
-                (course) => course.courseName === value,
-              )
-              if (course) {
-                setCourseDetails(course)
-              }
-            }}
-            searchable={true}
-          />
-          <Title order={3} c={'gray.7'}>
-            {courseDetails?.courseCode ? `[${courseDetails?.courseCode}]` : ''}{' '}
-            {courseDetails?.courseName}{' '}
-            {editorState.data && ` | ${editorState.data.title} `}
-          </Title>
-          <Group wrap="nowrap" gap={0}>
-            <Button
-              radius={0}
-              style={{
-                borderStartStartRadius: '4px',
-                borderEndStartRadius: '4px',
-              }}
-            >
-              Save
-            </Button>
-            <Divider orientation="vertical" />
-            <Menu
-              transitionProps={{ transition: 'pop' }}
-              position="bottom-end"
-              withinPortal
-            >
-              <Menu.Target>
+      <PanelGroup direction="horizontal" style={{ height: '100%' }}>
+        <Panel hidden={!isTreeVisible} minSize={15} defaultSize={20}>
+          <Box mt={'xs'}>
+            <Container>
+              <Group align="center" mb="md" gap={'xs'}>
                 <ActionIcon
-                  variant="filled"
-                  color={theme.primaryColor}
-                  size={36}
-                  radius={0}
-                  style={{
-                    borderStartEndRadius: '4px',
-                    borderEndEndRadius: '4px',
-                  }}
+                  onClick={() => setIsTreeVisible(!isTreeVisible)}
+                  bg={isTreeVisible ? 'dark.3' : 'transparent'}
                 >
-                  <IconChevronDown size={16} stroke={1.5} />
+                  <IconListTree
+                    size={18}
+                    color={isTreeVisible ? `white` : 'gray'}
+                  />
                 </ActionIcon>
-              </Menu.Target>
-              <Menu.Dropdown>
-                <Menu.Item
-                  leftSection={
-                    <IconRubberStamp
-                      size={16}
-                      stroke={1.5}
-                      color={theme.colors.blue[5]}
-                    />
-                  }
-                >
-                  Publish
-                </Menu.Item>
-                <Menu.Item
-                  leftSection={
-                    <IconCalendar
-                      size={16}
-                      stroke={1.5}
-                      color={theme.colors.blue[5]}
-                    />
-                  }
-                >
-                  Schedule publishing
-                </Menu.Item>
-                <Menu.Item
-                  leftSection={
-                    <IconTrash
-                      size={16}
-                      stroke={1.5}
-                      color={theme.colors.red[5]}
-                    />
-                  }
-                >
-                  Delete
-                </Menu.Item>
-              </Menu.Dropdown>
-            </Menu>
-          </Group>
-        </Group>
-        <PanelGroup direction="horizontal" style={{ height: '100%' }}>
-          <Stack gap={'xs'} p={'xs'} style={{ height: '100%' }}>
-            <ActionIcon
-              onClick={() => setIsTreeVisible(!isTreeVisible)}
-              bg={isTreeVisible ? 'dark.3' : 'transparent'}
-            >
-              <IconListTree
-                size={18}
-                color={isTreeVisible ? `white` : 'gray'}
-              />
-            </ActionIcon>
-          </Stack>
-          <Divider orientation="vertical" />
-          <Panel hidden={!isTreeVisible} minSize={0} defaultSize={20}>
-            <Box mt={'xs'}>
-              <Container>
-                <Group
-                  justify="space-between"
-                  align="center"
-                  mb="md"
-                  wrap={'nowrap'}
-                >
-                  <Title order={4} textWrap={'nowrap'}>
-                    Course Structure
-                  </Title>
-                  <Button
+                <Text fw={500}>Course Structure</Text>
+              </Group>
+              <Group align="center" mb="md" wrap={'nowrap'}>
+                <Select
+                  data={mockCourseBasicDetails.map(
+                    (course) => course.courseName,
+                  )}
+                  value={courseDetails?.courseName}
+                  onChange={(value) => {
+                    const course = mockCourseBasicDetails.find(
+                      (course) => course.courseName === value,
+                    )
+                    if (course) {
+                      setCourseDetails(course)
+                    }
+                  }}
+                  leftSection={<IconBook size={24} />}
+                  searchable={true}
+                  flex={1}
+                />
+                <Tooltip label={'Add Content'}>
+                  <ActionIcon
                     variant="default"
-                    radius={'md'}
-                    leftSection={<IconPlus size={18} />}
+                    radius={'sm'}
+                    size={32}
                     onClick={() => handleAdd()}
                   >
-                    Add Module
-                  </Button>
-                </Group>
+                    <IconPlus size={18} />
+                  </ActionIcon>
+                </Tooltip>
+              </Group>
 
-                <CourseTree
-                  onAddButtonClick={handleAdd}
-                  onEditButtonClick={handleEdit}
-                  courseData={courseData}
+              <CourseTree
+                onAddButtonClick={handleAdd}
+                onEditButtonClick={handleEdit}
+                courseData={courseData}
+              />
+            </Container>
+          </Box>
+        </Panel>
+        <PanelResizeHandle
+          onDragging={setIsDragging}
+          style={{
+            backgroundColor: isDragging
+              ? theme.colors.blue[2]
+              : theme.colors.gray[0],
+            borderRight: `1px solid ${theme.colors.gray[3]}`,
+            borderLeft: `1px solid ${theme.colors.gray[3]}`,
+            cursor: 'col-resize',
+            transition: 'background-color 0.3s ease',
+            width: '10px',
+            height: '100%',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+          }}
+          hidden={!isTreeVisible}
+        >
+          <IconGripVertical size={16} />
+        </PanelResizeHandle>
+        <Panel defaultSize={70} minSize={50}>
+          <Stack flex={'1 0 auto'} h={'100%'} w={'100%'} gap={0}>
+            <Group
+              justify="space-between"
+              align="center"
+              w={'100%'}
+              pb={'sm'}
+              style={{ borderBottom: `2px solid ${theme.colors.gray[3]}` }}
+            >
+              <Group gap={'md'} display={isTreeVisible ? 'none' : 'flex'}>
+                <ActionIcon
+                  onClick={() => setIsTreeVisible(!isTreeVisible)}
+                  bg={isTreeVisible ? 'dark.3' : 'transparent'}
+                >
+                  <IconListTree
+                    size={18}
+                    color={isTreeVisible ? `white` : 'gray'}
+                  />
+                </ActionIcon>
+                <Select
+                  data={mockCourseBasicDetails.map(
+                    (course) => course.courseName,
+                  )}
+                  value={courseDetails?.courseName}
+                  onChange={(value) => {
+                    const course = mockCourseBasicDetails.find(
+                      (course) => course.courseName === value,
+                    )
+                    if (course) {
+                      setCourseDetails(course)
+                    }
+                  }}
+                  leftSection={<IconBook size={24} />}
+                  searchable={true}
                 />
-              </Container>
-            </Box>
-          </Panel>
-          <PanelResizeHandle
-            onDragging={setIsDragging}
-            style={{
-              backgroundColor: isDragging
-                ? theme.colors.blue[2]
-                : theme.colors.gray[0],
-              borderRight: `1px solid ${theme.colors.gray[3]}`,
-              borderLeft: `1px solid ${theme.colors.gray[3]}`,
-              cursor: 'col-resize',
-              transition: 'background-color 0.3s ease',
-              width: '10px',
-              height: '100%',
-              display: 'flex',
-              alignItems: 'center',
-              justifyContent: 'center',
-            }}
-            hidden={!isTreeVisible}
-          >
-            <IconGripVertical size={16} />
-          </PanelResizeHandle>
-          <Panel defaultSize={70} minSize={50}>
+              </Group>
+              <Title order={3} c={'gray.7'}>
+                {courseDetails?.courseCode
+                  ? `[${courseDetails?.courseCode}]`
+                  : ''}{' '}
+                {courseDetails?.courseName}{' '}
+                {editorState.data && ` | ${editorState.data.title} `}
+              </Title>
+              <Group wrap="nowrap" gap={0}>
+                <Button
+                  radius={0}
+                  style={{
+                    borderStartStartRadius: '4px',
+                    borderEndStartRadius: '4px',
+                  }}
+                >
+                  Save
+                </Button>
+                <Divider orientation="vertical" />
+                <Menu
+                  transitionProps={{ transition: 'pop' }}
+                  position="bottom-end"
+                  withinPortal
+                >
+                  <Menu.Target>
+                    <ActionIcon
+                      variant="filled"
+                      color={theme.primaryColor}
+                      size={36}
+                      radius={0}
+                      style={{
+                        borderStartEndRadius: '4px',
+                        borderEndEndRadius: '4px',
+                      }}
+                    >
+                      <IconChevronDown size={16} stroke={1.5} />
+                    </ActionIcon>
+                  </Menu.Target>
+                  <Menu.Dropdown>
+                    <Menu.Item
+                      leftSection={
+                        <IconRubberStamp
+                          size={16}
+                          stroke={1.5}
+                          color={theme.colors.blue[5]}
+                        />
+                      }
+                    >
+                      Publish
+                    </Menu.Item>
+                    <Menu.Item
+                      leftSection={
+                        <IconCalendar
+                          size={16}
+                          stroke={1.5}
+                          color={theme.colors.blue[5]}
+                        />
+                      }
+                    >
+                      Schedule publishing
+                    </Menu.Item>
+                    <Menu.Item
+                      leftSection={
+                        <IconTrash
+                          size={16}
+                          stroke={1.5}
+                          color={theme.colors.red[5]}
+                        />
+                      }
+                    >
+                      Delete
+                    </Menu.Item>
+                  </Menu.Dropdown>
+                </Menu>
+              </Group>
+            </Group>
             <Box
               h="100%"
               style={{ display: 'flex', flexDirection: 'column', minHeight: 0 }}
@@ -296,33 +325,33 @@ const CMS = ({ courseCode }: CMSProps) => {
                 onUpdate={(newContent) => console.log('Updated:', newContent)}
               />
             </Box>
-          </Panel>
-        </PanelGroup>
-        <Group
-          justify={'space-between'}
-          style={{
-            borderTop: `1px solid ${theme.colors.gray[3]}`,
-            padding: theme.spacing.xs,
-            backgroundColor: theme.white,
-          }}
-        >
+          </Stack>
+        </Panel>
+      </PanelGroup>
+      <Group
+        justify={'space-between'}
+        style={{
+          borderTop: `1px solid ${theme.colors.gray[3]}`,
+          padding: theme.spacing.xs,
+          backgroundColor: theme.white,
+        }}
+      >
+        <Text size="sm" c="dimmed">
+          {editorState.mode === 'edit' ? 'Editing' : 'Creating'}{' '}
+          {editorState.type}
+        </Text>
+        <Group gap="xs">
           <Text size="sm" c="dimmed">
-            {editorState.mode === 'edit' ? 'Editing' : 'Creating'}{' '}
-            {editorState.type}
+            Last saved: 2 minutes ago
           </Text>
-          <Group gap="xs">
-            <Text size="sm" c="dimmed">
-              Last saved: 2 minutes ago
-            </Text>
-            <Text size="sm" c={theme.colors.blue[6]}>
-              •
-            </Text>
-            <Text size="sm" c="dimmed">
-              Course Version: 1.0.0
-            </Text>
-          </Group>
+          <Text size="sm" c={theme.colors.blue[6]}>
+            •
+          </Text>
+          <Text size="sm" c="dimmed">
+            Course Version: 1.0.0
+          </Text>
         </Group>
-      </Stack>
+      </Group>
     </Box>
   )
 }
