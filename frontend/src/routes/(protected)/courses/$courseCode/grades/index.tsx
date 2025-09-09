29b1bd7bad949344b5e645a06f8fee82bd6296e0
Morfusee <markvalenzuela.coding@gmail.com>
2025-09-10 05:54:02 +0800

    refactor: rename course-grades to index

--- frontend/src/routes/(protected)/courses/$courseCode/grades/index.tsx
+++ frontend/src/routes/(protected)/courses/$courseCode/grades/index.tsx
@@ -1,5 +1,5 @@
+import CourseGrades from '@/pages/shared/courses/$courseCode/grades'
 import { createFileRoute } from '@tanstack/react-router'
-import CourseGrades from '@/pages/shared/courses/$courseCode/grades/course-grades.tsx'
 
 export const Route = createFileRoute(
   '/(protected)/courses/$courseCode/grades/',
