661d7276dbc603c276ff4664eac160d4ff18bf08
Morfusee <markvalenzuela.coding@gmail.com>
2025-09-10 04:01:32 +0800

    refactor: migrate module items create page index files to /pages

--- frontend/src/routes/(protected)/courses/$courseCode/modules/$itemId/create.tsx
+++ frontend/src/routes/(protected)/courses/$courseCode/modules/$itemId/create.tsx
@@ -1,7 +1,7 @@
-import { createFileRoute, useParams } from '@tanstack/react-router'
-import { useAuth } from '@/features/auth/auth.hook.ts'
 import RoleComponentManager from '@/components/role-component-manager.tsx'
-import { CMS } from '@/features/courses/cms/cms.tsx'
+import { useAuth } from '@/features/auth/auth.hook.ts'
+import ModulesItemCreatePage from '@/pages/admin/courses/$courseCode/modules/$itemId/create'
+import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute(
   '/(protected)/courses/$courseCode/modules/$itemId/create',
@@ -11,12 +11,12 @@ export const Route = createFileRoute(
 
 function RouteComponent() {
   const { authUser } = useAuth('protected')
-  const { courseCode } = useParams({ strict: false })
+  
   return (
     <RoleComponentManager
       currentRole={authUser.role}
       roleRender={{
-        admin: <CMS courseCode={courseCode} />,
+        admin: <ModulesItemCreatePage />,
       }}
     />
   )
