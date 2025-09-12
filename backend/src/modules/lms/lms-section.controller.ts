2cf7609bf87fe567f394ff3e889b170404a2f265
Harvey Dela Flor <harvey.delaflor@gmail.com>
2025-09-13 03:27:19 +0800

    refactor(lms): refactor LMS controller base paths
    Updates the base paths for `LmsController` and `LmsSectionController`
    to be more consistent and RESTful.
    - Renames `lmsController` to `lms`.
    - Renames `lmsContent` to `lms/:lmsId/sections`.

--- backend/src/modules/lms/lms-section.controller.ts
+++ backend/src/modules/lms/lms-section.controller.ts
@@ -1,7 +1,7 @@
 import { Controller } from '@nestjs/common';
 import { LmsSectionService } from '@/modules/lms/lms-section.service';
 
-@Controller('lmsContent')
+@Controller('lms/:lmsId/sections')
 export class LmsSectionController {
   constructor(private readonly lmsSectionService: LmsSectionService) {}
 }
