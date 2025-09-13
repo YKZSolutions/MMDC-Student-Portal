3899cb146a19dc275e10ca8891608ddd5528a364
Harvey Dela Flor <harvey.delaflor@gmail.com>
2025-09-14 02:00:05 +0800

    refactor(prisma): rename `GradeRecord` model to `AssignmentGradeRecord`
    This clarifies that the model is specifically for assignment
    grades, improving domain clarity.

---
+++ backend/src/generated/nestjs-dto/assignmentGradeRecord.dto.ts
@@ -0,0 +1,38 @@
+import { Prisma } from '@prisma/client';
+import { ApiProperty } from '@nestjs/swagger';
+
+export class AssignmentGradeRecordDto {
+  @ApiProperty({
+    type: 'string',
+  })
+  id: string;
+  @ApiProperty({
+    type: 'string',
+    format: 'Decimal.js',
+  })
+  rawScore: Prisma.Decimal;
+  @ApiProperty({
+    type: 'string',
+    format: 'Decimal.js',
+  })
+  finalScore: Prisma.Decimal;
+  @ApiProperty({
+    type: 'string',
+  })
+  grade: string;
+  @ApiProperty({
+    type: 'string',
+    nullable: true,
+  })
+  feedback: string | null;
+  @ApiProperty({
+    type: 'string',
+    format: 'date-time',
+  })
+  gradedAt: Date;
+  @ApiProperty({
+    type: 'string',
+    format: 'date-time',
+  })
+  updatedAt: Date;
+}
