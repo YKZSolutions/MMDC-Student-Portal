538b9354aa6c362dc40991e01f46b2f45f04693f
ibrahim <iharby2002@gmail.com>
2025-09-16 15:45:07 +0800

    Feat: add DTOs for group creation, update, and details

---
+++ backend/src/modules/lms/group/dto/detailed-group.dto.ts
@@ -0,0 +1,27 @@
+import { GroupDto } from '@/generated/nestjs-dto/group.dto';
+import { ApiProperty, PickType } from '@nestjs/swagger';
+
+export class StudentInfoDto {
+  @ApiProperty({ type: 'string' })
+  firstName: string;
+
+  @ApiProperty({ type: 'string' })
+  lastName: string;
+}
+
+export class GroupMemberDto {
+  @ApiProperty({ type: 'string', format: 'uuid' })
+  studentId: string;
+
+  @ApiProperty({ type: () => StudentInfoDto })
+  user: StudentInfoDto;
+}
+
+export class DetailedGroupDto extends PickType(GroupDto, [
+  'id',
+  'groupNumber',
+  'groupName',
+]) {
+  @ApiProperty({ type: () => [GroupMemberDto] })
+  members: GroupMemberDto[];
+}
