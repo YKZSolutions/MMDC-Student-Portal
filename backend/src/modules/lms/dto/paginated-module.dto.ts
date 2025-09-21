edefa9baa9b114e1b5a83151d257051eb11290df
Morfusee <markvalenzuela.coding@gmail.com>
2025-09-21 15:39:17 +0800

    feat (dto): add enrollmentPeriod to CustomDetailedCourseOfferingDto

--- backend/src/modules/lms/dto/paginated-module.dto.ts
+++ backend/src/modules/lms/dto/paginated-module.dto.ts
@@ -1,4 +1,5 @@
 import { PaginatedDto } from '@/common/dto/paginated.dto';
+import { EnrollmentPeriodDto } from '@/generated/nestjs-dto/enrollmentPeriod.dto';
 import { ModuleDto } from '@/generated/nestjs-dto/module.dto';
 import { DetailedCourseOfferingDto } from '@/modules/enrollment/dto/paginated-course-offering.dto';
 import { ApiProperty, OmitType } from '@nestjs/swagger';
@@ -7,7 +8,9 @@ import { ApiProperty, OmitType } from '@nestjs/swagger';
 export class CustomDetailedCourseOfferingDto extends OmitType(
   DetailedCourseOfferingDto,
   ['courseEnrollments'],
-) {}
+) {
+  enrollmentPeriod?: EnrollmentPeriodDto | null;
+}
 
 // Module DTO with optional course offering details
 export class DetailedModulesDto extends ModuleDto {
