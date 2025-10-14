2a06000f0ba3c48fe949a92baba9d82bc475969d
Morfusee <markvalenzuela.coding@gmail.com>
2025-10-15 01:33:52 +0800

    feat (fe): remove student and mentor dashboard and update navigation logic for login page

--- frontend/src/routes/(protected)/dashboard/index.tsx
+++ frontend/src/routes/(protected)/dashboard/index.tsx
@@ -1,7 +1,6 @@
 import RoleComponentManager from '@/components/role-component-manager'
 import { useAuth } from '@/features/auth/auth.hook'
 import AdminDashboard from '@/pages/admin/dashboard/dashboard.admin'
-import StudentDashboard from '@/pages/student/dashboard/dashboard.student'
 import { createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/(protected)/dashboard/')({
@@ -15,7 +14,6 @@ function RouteComponent() {
     <RoleComponentManager
       currentRole={authUser.role}
       roleRender={{
-        student: <StudentDashboard />,
         admin: <AdminDashboard />,
       }}
     />
