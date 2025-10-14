a9fcb785c29aa9f24f1bf30602ae0a7b41729e13
Harvey Dela Flor <harvey.delaflor@gmail.com>
2025-10-15 07:06:17 +0800

    refactor(lms): remove `grades.ts` helper file

--- backend/src/common/helpers/grades.ts
+++
@@ -1,9 +0,0 @@
-import { RubricDto } from '@/modules/lms/rubric/dto/assignment-criteria.dto';
-
-export const calculateMaxScoreFromRubric = (rubric: RubricDto) => {
-  let maxScore = 0;
-  rubric.criteria.forEach((criterion) => {
-    maxScore += criterion.points;
-  });
-  return maxScore;
-};
